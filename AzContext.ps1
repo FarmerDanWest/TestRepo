94e9dc647a9c7ef54a1f49ee6c9d5aa7946f216b
FarmerDanWest <40077839+FarmerDanWest@users.noreply.github.com>
2018-06-08 18:50:07 -0700

    Update AzContext.ps1

--- AzContext.ps1
+++ AzContext.ps1
@@ -1,8 +1,24 @@
+#first you need to login to Azure
+
 $cred = get-credential
 Login-AzureRmAccount -Credential $cred
 
+#Then you need to figure out which sub to use and get out the ID
+
 Get-AzureRmSubscription 
 
+#then select the subscription you would like to work with
+
 Select-AzureRmSubscription -SubscriptionId 
+
+#then find the storage acct you want to use as default
+
+Get-AzureRmStorageAccount|ft storageaccountname, resourcegroupname
+
+#Then set the default storage acct
+
+Set-AzureRmCurrentStorageAccount -ResourceGroupName "StorageRG" -StorageAccountName "StorageName" 
+
+#Finally we can see what has been set as our context
 
 get-azurecontext
